d046af5f36b32152a0bc9c2c820d4955e3232978
Bragatte <marcelobragatte@gmail.com>
2020-12-06 17:26:31 -0300

    link up

--- How_to/app.py
+++ How_to/app.py
@@ -1,5 +1,6 @@
 ##Varrer preços com código
 #[Ref](https://www.youtube.com/watch?v=5M-kZf_s6a0)
+#[Ref2](https://www.youtube.com/watch?v=ow8514gnajw)
 #2. Pesquisar por produto
 #3. Extrair o título e preço
 #4. Navegar próxima página
@@ -9,6 +10,8 @@ import os
 #0. Abrir navegador
 #ativar no chrome, OS para navegar pelo SO forma dinâmica
 #getcwd(dir atual)+os.sep(/separador)+ 'chromedriver.exe'
-driver = webdriver.Chrome(executable_path=os.getcwd()+os.sep+ 'chromedriver.exe')
+#driver = webdriver.Chrome(executable_path=os.getcwd()+os.sep+ 'chromedriver')
+driver = webdriver.Chrome(executable_path=r"/home/bragatte/Documentos/GitHub/Python_Learning/How_to")
 #1. Navegar até o site (url)
-driver.get("https://www.mercadolivre.com")
+#driver.get("https://www.mercadolivre.com")
+driver.get("https://www.google.com")
