5aee6d805e1635b58cb568c6934ebd731e3fb001
Bragatte <marcelobragatte@gmail.com>
2021-04-03 16:00:31 -0300

    DashPlotly_autoupdate

--- Research/DisfErect/Dash_DisfErect.py
+++ Research/DisfErect/Dash_DisfErect.py
@@ -9,51 +9,88 @@ import dash_core_components as dcc
 
 #Live de Python version
 from dash import Dash
+from dash_html_components import Div, H1, P, H3
+from dash_core_components import (
+    Graph, Dropdown, Slider,
+    Checklist, Interval
+)
 from dash.dependencies import Input, Output
-from dash_core_components import Input as dcc_input 
-from dash_html_components import Div, H1, H2, H3, P, Br
+from random import randint
 
-##CSS options link
-external_stylesheets = [
-    'https://unpkg.com/termina.css@0.7.2/dist/terminal.min.css'
-]
 
-#Call Dash
-app = Dash(__name__,external_stylesheets=external_stylesheets)
+app = Dash(__name__)
+
+
+N = 20
+
+database = {
+    'index': [],
+    'maiores': [],
+    'menores': [],
+    'bebes': [],
+}
+
 
 app.layout = Div(
-    children=[ 
-    #Intro
-        H1('Tibial Nerve Stimulation add on Pelvic Muscle Training for Postprostatectomy Urinary Incontinence'),
-        P('Example of Erectile Dysfunction Database    Data collection carried out by the researcher Fernandes, J.A.'),
-        P('Created by Bragatte 202102061700'),
-    #Plots
-        H2('Plots'),
-        #Graph( ),
-
-    #Updates
-        dcc_input(id='meu_input1', value='results'),
-        dcc_input(id='meu_input2', value='Right-Again'),
-        P(id='output1'),
-        P(id='output2'),
-        
-     ]
- )
-
-@app.callback(
-    [
-        Output('output1', 'children'),
-        Output('output2', 'children')
-
-    ],
-    [
-        Input('meu_input1','value'),
-        Input('meu_input2','value'),
-
-    ],
+    children=[
+        H1('Evento X'),
+        H3('idade das pessoas que foram ao evento'),
+        Interval(id='interval'),
+        Checklist(
+            id='meu_check_list',
+            options=[
+                {'label': 'Menores de Idade', 'value': 'menores'},
+                {'label': 'Bebes', 'value': 'bebes'},
+                {'label': 'Maiores de idade', 'value': 'maiores'}
+            ],
+            value=['bebes']
+        ),
+        Dropdown(
+            id='meu_dropdown',
+            options=[
+                {'label': 'Linha', 'value': 'line'},
+                {'label': 'Barra', 'value': 'bar'},
+            ],
+            value='line'
+        ),
+        Graph(
+            id='meu_grafico',
+            config={'displayModeBar': False},
+        )
+    ]
 )
 
-def meu_callback(meu_input1, meu_input2):
-    return meu_input1, meu_input2
+
+def update_database(value):
+    """Minha query / Atualização do pandas."""
+    database['index'].append(value)
+    database['menores'].append(randint(1, 200))
+    database['maiores'].append(randint(1, 200))
+    database['bebes'].append(randint(1, 200))
+
+
+@app.callback(
+    Output('meu_grafico', 'figure'),
+    [
+        Input('meu_check_list', 'value'),
+        Input('meu_dropdown', 'value'),
+        Input('interval', 'n_intervals'),
+    ]
+)
+def my_callback(input_data, graph_type, n_intervals):
+    update_database(n_intervals)
+    grafico = {
+        'data': []
+    }
+    for x in input_data:
+        grafico['data'].append(
+            {
+                'y': database[x][-20:],
+                'x': database['index'][-20:],
+                'name': x,
+                'type': graph_type
+            },
+        )
+    return grafico
 
 app.run_server(debug=True)
