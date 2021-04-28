9a3021cbc992803b25dc3b2c83fd41ca454d369c
Bragatte <marcelobragatte@gmail.com>
2021-04-28 17:43:54 -0300

    Delete How_to/.ipynb_checkpoints directory

--- How_to/.ipynb_checkpoints/How_to_Altair_Viz-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import altair as alt
-cols = ['Type','Price','Distance','Date','Landsize','Regionname']
-melb = pd.read_csv(
-   "/content/melb_data.csv", usecols = cols, parse_dates = ['Date']
-).sample(n=1000).reset_index(drop=True)
-melb.head()
